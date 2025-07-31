8dd9663d0c16e6566fd185acfacfeacb9a3da474
Ajalsr <131275684+Ajalsr@users.noreply.github.com>
2025-07-31 21:13:11 +0400

    added the signup api

--- backend/models/users.go
+++ backend/models/users.go
@@ -5,5 +5,5 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Users struct {
 	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	UserID   string             `json:"userId" bson:"userId" binding:"required"`
-	Password string             `json:"password" bson:"pssword" binding:"required"`
+	Password string             `json:"password" bson:"password" binding:"required"`
 }
