b23151245f9dd2141ff184106cab752eb7258856
Ajalsr <ajalsr989@gmail.com>
2025-08-24 12:31:52 +0400

    done the new changes

--- backend/main.go
+++ backend/main.go
@@ -9,9 +9,27 @@ import (
 	"github.com/backend/routes"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5175")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 
 	server := gin.Default()
+
+	server.Use(CORSMiddleware())
 	routes.StockRoutes(server)
 	routes.AuthRoutes(server)
 
